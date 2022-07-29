999ec20af985e865582c4810eec3c7a04cafad4a
luksa <devopscoder331@gmail.com>
2022-07-29 08:23:17 +0300

    Initial commit

---
+++ infra/terraform/yandex_cloud/main.tf
@@ -0,0 +1,69 @@
+// Create a new instance
+resource "yandex_compute_instance" "vps" {
+  count           = var.vps_count
+  name            = "${var.name}${count.index + 1}"
+  platform_id     = var.platform_id
+  zone            = var.zone
+
+  resources {
+    core_fraction   = var.instance_fraction
+    cores           = var.instance_cores
+    memory          = var.instance_memory
+    
+  }
+
+  scheduling_policy {
+    preemptible     = var.instance_preemptible
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.image_id
+      size = var.disk_size
+    }
+  }
+
+  network_interface {
+    subnet_id = data.yandex_vpc_subnet.lab-subnet-a.id
+    nat       = true
+  }
+
+  metadata = {
+    ansible_ssh = var.ssh_private_key
+    ssh-keys = "ubuntu:${file(var.ssh_public_key)}"
+  }
+
+  labels = {
+    task_name = var.task_name
+    user_email = var.user_email
+  }
+}
+
+// data resource yandex_vpc_subnet
+data "yandex_vpc_subnet" "lab-subnet-a" {
+  name = var.lab-subnet-a
+}
+
+// generate inventory file for Ansible
+resource "local_file" "ansible_inventory" {
+  ## hosts file for ansible
+  filename = "${path.module}/../../inventory/hosts.ini"
+
+  # template for generate host file
+  content = templatefile("${path.module}/templates/kubernetes_hosts.tmpl",
+  {
+      vps = yandex_compute_instance.vps
+      vps_name = yandex_compute_instance.vps.*.name,
+      vps_ip = yandex_compute_instance.vps.*.network_interface.0.ip_address
+      vps_nat_ip = yandex_compute_instance.vps[*].network_interface.0.nat_ip_address
+      ansible_user = var.ssh_user
+      ansible_ssh = yandex_compute_instance.vps[*].metadata.ansible_ssh
+  }
+  )
+}
+
+// Print Output
+output "nat_ip_address" {
+  value = yandex_compute_instance.vps[*].network_interface.0.nat_ip_address
+  
+}
---
+++ infra/terraform/yandex_cloud/variables.tf
@@ -0,0 +1,107 @@
+#------------------------------------------------
+# Variables for connect to Providers
+#------------------------------------------------
+
+variable "key_file" {
+    description = "Contains either a path to or the contents of the Service Account file in JSON format"
+    type = string
+}
+
+variable "cloud_id" {
+    description = "The ID of the cloud to apply any resources to"
+    type = string
+}
+
+variable "folder_id" {
+    description = "The ID of the folder to operate under, if not specified by a given resource"
+    type = string
+}
+
+variable "zone_id" {
+    description = "The default availability zone to operate under, if not specified by a given resource"
+    type = string
+}
+
+#------------------------------------------------
+# Variables for connect to Providers
+#------------------------------------------------
+
+variable "name" {
+    description = "name"
+    type = string
+}
+
+variable "vps_count" {
+    description = "vps_count"
+    type = string
+}
+
+variable "platform_id" {
+    description = "The type of virtual machine to create. The default is 'standard-v1'"
+    type = string
+}
+
+variable "zone" {
+    description = "The availability zone where the virtual machine will be created. If it is not provided, the default provider folder is used."
+    type = string
+}
+
+variable "image_id" {
+    description = "A disk image to initialize this disk from"
+    type = string
+}
+
+variable "instance_fraction" {
+    description = "If provided, specifies baseline performance for a core as a percent."
+    type = string
+}
+
+variable "instance_cores" {
+    description = "CPU cores for the instance"
+    type = string
+}
+
+variable "instance_memory" {
+    description = "Memory size in GB"
+    type = string
+}
+
+variable "instance_preemptible" {
+    description = "Specifies if the instance is preemptible. Defaults to false."
+    type = string
+}
+
+variable "disk_size" {
+    description = "Size of the disk in GB."
+    type = string
+}
+
+variable "ssh_private_key" {
+    description = "ssh_private_key for ansible installations"
+    type = string
+}
+
+variable "ssh_public_key" {
+    description = "ssh_public_key for ansible installations"
+    type = string
+}
+
+variable "ssh_user" {
+    description = "ssh_user who use for for ansible installations"
+    type = string
+}
+
+variable "task_name" {
+    description = "task_name"
+    type = string
+}
+
+variable "user_email" {
+    description = "user_email"
+    type = string
+}
+
+variable "lab-subnet-a" {
+    description = "ID of the subnet to attach this interface to. The subnet must exist in the same zone where this instance will be created"
+    type = string
+}
